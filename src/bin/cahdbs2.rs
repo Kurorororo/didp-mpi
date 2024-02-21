3d3ebffc1a56166bcbc703cb89acf4e5a6844f55
RK <mhgeoe@gmail.com>
2024-02-20 19:08:02 -0500

    correct cost

--- src/bin/cahdbs2.rs
+++ src/bin/cahdbs2.rs
@@ -107,7 +107,10 @@ fn main_with_cost_type_and_hash_function<T, H>(
             let mut file = OpenOptions::new().append(true).open("history.csv").unwrap();
             let line = format!(
                 "{}, {}, {}, {}\n",
-                solution.time, cost, solution.expanded, solution.generated
+                solution.time,
+                solution.cost.unwrap(),
+                solution.expanded,
+                solution.generated
             );
             file.write_all(line.as_bytes()).unwrap();
         }
